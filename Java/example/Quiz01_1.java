2ef9d814637c812c2d17b511eed14a880a8b13f1
pkd <baum12345@naver.com>
2023-03-08 22:37:27 +0900

    퀴즈01 배열이용 문자열 합치기

---
+++ Java/example/Quiz01_1.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Quiz01_1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String [] names = new String[n+1];
+
+        names[0] = "hello ";
+        for (int i = 1; i < n+1; i++) {
+            String input = sc.next();
+
+            if (i == n) {
+                names[i] = input + ".";
+            } else {
+                names[i] = input + ",";
+            }
+        }
+        for (String name : names){
+            System.out.print(name);
+        }
+    }
+}
