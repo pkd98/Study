6a728d978cc21c8458bd0f69fea3acc254008ec6
PKD <baum12345@naver.com>
2023-03-14 17:05:17 +0900

    ch9 수정

--- Java/JavaCh9/src/Exam/Main.java
+++ Java/JavaCh9/src/Exam/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Cleric cleric1 = new Cleric("아서스", 40, 5);
         Cleric cleric2 = new Cleric("아서스", 35);
         Cleric cleric3 = new Cleric("아서스");
-
+        
         System.out.println("이름 : " + cleric1.name + ", HP : " + cleric1.HP + ", MP : " + cleric1.MP);
         System.out.println("이름 : " + cleric2.name + ", HP : " + cleric2.HP + ", MP : " + cleric2.MP);
         System.out.println("이름 : " + cleric3.name + ", HP : " + cleric3.HP + ", MP : " + cleric3.MP);
