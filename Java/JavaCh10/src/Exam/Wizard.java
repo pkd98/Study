d190f806c518cd7ed38e4ff1493513289c760d6d
PKD <baum12345@naver.com>
2023-03-14 17:28:11 +0900

    Exam10-2 modified

--- Java/JavaCh10/src/Exam/Wizard.java
+++ Java/JavaCh10/src/Exam/Wizard.java
@@ -40,7 +40,7 @@ public class Wizard {
 
     public void heal(Hero hero) {
         int basePoint = 10; // 기본 회복 포인트
-        int recovPoint = (int) (basePoint * this.wand.power); // 지팡이에 의한 증폭
+        int recovPoint = (int) (basePoint * this.wand.getPower()); // 지팡이에 의한 증폭
         hero.setHp(hero.getHp() + recovPoint); // 용사의 HP를 회복
     }
 }
